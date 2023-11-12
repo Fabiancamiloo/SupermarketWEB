48dbe77069c04f73602eeec218b5af3dde8e8958
Carlos fabian Caicedo <camilocaicedocarlosfabian@gmail.com>
2023-11-12 16:48:59 -0500

    message

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        // [Key] -> Anotacion si la propiedad no se llama Id, ejemplo ProducId
+        public int Id { get; set; } // Sera la llave promaria
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+        public int CategoryId { get; set; } // Sera la llave foranea
+        public Category Category { get; set; } // Propiedad de navegacion
+    }
+}
